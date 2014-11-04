f5f97c20ce5691a68323ded264aea96267353539
Wilson Mitchell <wilsonmitchell92@gmail.com>
2014-11-04 14:38:35 -0500

    Getting and displaying location now.

--- app/src/main/java/me/wilsonmitchell/androidworkshop/GPSActivity.java
+++ app/src/main/java/me/wilsonmitchell/androidworkshop/GPSActivity.java
@@ -1,39 +1,70 @@
 package me.wilsonmitchell.androidworkshop;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class GPSActivity extends ActionBarActivity {
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private Location lastKnownLocation;
+
+    private TextView longitudeTextView;
+    private TextView latitudeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps);
-    }
 
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.g, menu);
-        return true;
+        longitudeTextView = (TextView) findViewById(R.id.longitudeTextView);
+        latitudeTextView = (TextView) findViewById(R.id.latitudeTextView);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onResume() {
+        super.onResume();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        // Define a listener that responds to location updates
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                setLastKnownLocation(location);
+            }
 
-        return super.onOptionsItemSelected(item);
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        locationManager.removeUpdates(locationListener);
+    }
+
+    private void setLastKnownLocation(Location location) {
+        lastKnownLocation = location;
+
+        longitudeTextView.setText(String.valueOf(lastKnownLocation.getLongitude()));
+        latitudeTextView.setText(String.valueOf(lastKnownLocation.getLatitude()));
     }
 }
