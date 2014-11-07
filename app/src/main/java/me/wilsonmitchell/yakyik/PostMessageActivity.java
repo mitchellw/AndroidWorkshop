dbdffd395abb935eabe8b9b6f57f7a6d5cf95b05
Wilson Mitchell <wilsonmitchell92@gmail.com>
2014-11-07 17:43:43 -0500

    Added post message activity + layout.

---
+++ app/src/main/java/me/wilsonmitchell/yakyik/PostMessageActivity.java
@@ -0,0 +1,22 @@
+package me.wilsonmitchell.yakyik;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class PostMessageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_message);
+    }
+
+
+    public void onSubmitButton(View v) {
+        
+    }
+}
